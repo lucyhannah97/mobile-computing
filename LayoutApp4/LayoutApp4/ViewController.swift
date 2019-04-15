b3b5523d459ffb3f198ff2dd0b4b89abf3dd0445
lucyhannah97 <L.H.Rogers@student.liverpool.ac.uk>
2019-04-15 11:09:18 +0100

    Extended work on layouts
    Work on horizontal and vertical layouts, understanding use of constraints, also some work on changing language dependent upon user's location

---
+++ LayoutApp4/LayoutApp4/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LayoutApp4
+//
+//  Created by Lucy Rogers on 15/11/2018.
+//  Copyright © 2018 uk.ac.u5lr. All rights reserved.
+//
+//  Student: Lucy Rogers
+//  Student ID: 201078869
+//  COMP327 - Mobile Computing
+//  Assignment 3 - App Portfolio
+//  App 5 - Layout App
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
